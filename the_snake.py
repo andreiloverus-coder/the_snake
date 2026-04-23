a2c4d1917bf7ba359d480ce0eadd30b5cd9fb7e6
Ruslan Sadkevich <andreiloverus@gmail.com>
2026-04-23 23:13:00 +0300

    Добавлен класс  Apple

--- the_snake.py
+++ the_snake.py
@@ -70,9 +70,30 @@ class Apple(GameObject):
     
     self.radomize_position():
     
-    def randomize_position(self)
+    def randomize_position(self):
+        """Устанавливает случайные координаты для яблока
+        """
+        max_x =  640- 20
+        max_y = 480 - 20
 
+        x.random.randrange(max_x + 1, 20)
+        y.random.randrange(max_y + 1, 20)
+        
+        self.position - (x,y)
 
+    def draw(self,surface):
+        """
+        Отрисовывает яблоко на игровом поле
+        """
+
+        rect = pygame.Rect(
+             self.position[0]
+             self.position[1]
+             20,
+             20
+         )
+
+        pygame.draw.rect(surface, self.body_color, rect)
 
 def main():
     # Инициализация PyGame:
